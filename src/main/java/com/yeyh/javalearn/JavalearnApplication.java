250b2c117593e1e2af99dc0a5bacb4b932ca9721
yeyh <1113531030@qq.com>
2021-04-28 23:15:22 +0800

    初始化

---
+++ src/main/java/com/yeyh/javalearn/JavalearnApplication.java
@@ -0,0 +1,13 @@
+package com.yeyh.javalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalearnApplication.class, args);
+	}
+
+}
